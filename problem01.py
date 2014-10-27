b2a81a4c9d077090ad60e3fe106aefa20313589f
Petr Sedlacek <petr@sedlacek.biz>
2014-10-27 18:04:18 +0100

    Solution to Problem 1

---
+++ problem01.py
@@ -0,0 +1,15 @@
+"""Project Euler Problem 1
+
+Find the sum of all the multiples of 3 or 5 below 1000.
+"""
+
+below = 1000
+result = 0
+
+for m in range(3, below, 3):
+    result += m
+for m in range(5, below, 5):
+    if m % 3 > 0:
+        result += m
+
+print "Sum of multiples of 3 or 5 below %d is %d" % (below, result)
