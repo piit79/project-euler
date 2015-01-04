9b3dbbada6e532ee547f100032ffa2c6a2434106
Petr Sedlacek <petr@sedlacek.biz>
2015-01-04 11:28:15 +0100

    Add comment

--- problem01.py
+++ problem01.py
@@ -9,7 +9,7 @@ result = 0
 for m in range(3, below, 3):
     result += m
 for m in range(5, below, 5):
-    if m % 3 > 0:
+    if m % 3 > 0:  # skip multiples of 3 as they have already been added above
         result += m
 
 print "Sum of multiples of 3 or 5 below %d is %d" % (below, result)
