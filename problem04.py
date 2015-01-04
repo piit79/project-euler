5bcb53caff14c3b8ddfb882579f708462db278a4
Petr Sedlacek <petr@sedlacek.biz>
2015-01-04 11:33:27 +0100

    Add comments; print the factors

--- problem04.py
+++ problem04.py
@@ -7,13 +7,15 @@ Find the largest palindrome made from the product of two 3-digit numbers.
 def is_palindrome(number):
     num_str = str(number)
     l = len(num_str)
-    m = l // 2
+    m = l // 2   # the 'midpoint'
+    """Compare the first half of the string with the reversed second half"""
     return num_str[0:m] == num_str[l:l-m-1:-1]
 
 
 largest = 999
 smallest = 101  # we can skip 100 as numbers ending in 0 can't be palindromes
 result = 0
+k = l = 0
 
 i = largest
 while i > smallest:
@@ -24,9 +26,11 @@ while i > smallest:
             break
         if is_palindrome(n):
             result = n
+            k = i
+            l = j
             break
         j -= 1
     i -= 1
 
 print "The largest palindrome made from the product of \
-two 3-digit numbers is %d" % result
+two 3-digit numbers is %d = %d x %d" % (result, k, l)
